fc4784d099ee4b2f02a36d3e19f7990bf05c0d50
shadow140312-max <shadow140312@gmail.com>
2026-07-01 20:09:52 +0530

    just planning

---
+++ myproject/thinking.java
@@ -0,0 +1,33 @@
+/*Thinking
+whats my goal 
+Goal- Make an equation sovler 
+ -find values of variables 
+  -that can seperate varaibles
+  -solve normla arthemetic equations
+  -solve quadratic equation 
+
+so  what shoudl i make i should make fundamental then using them i shodul 
+   do things first 
+   -my code shoudl solve all the constants
+   -then all the varables
+   so how does an human solve it  
+     setp 1 identify constants
+     -add them up 
+     -done
+     89x-34x-90l-90 so these are expressiosn sepertaed by =,-,etc.. 
+my function can iterate teh whole string it and it can seperate constants into another list and add them or make another array and add
+everything in that array 
+
+to calculate varaibles i should first seperate all the expression then add teh const then tehn i shoudl reove all 
+
+
+
+
+
+
+
+
+
+
+
+*/
